5291abc633ba50eaefe3008b44963edf00b5312b
0-BSCode <bryanipsanchez@gmail.com>
2024-01-01 16:01:55 +0800

    Initialize repo

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000; // Or get port from environment variable
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hi from Express + TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
