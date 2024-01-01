3d425c9c5526682b603b26186f567eb04e1fb3d1
0-BSCode <bryanipsanchez@gmail.com>
2024-01-01 16:53:01 +0800

    Integrate eslint into server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000; // Or get port from environment variable
 
+const test = 3;
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hi from Express + TypeScript!');
 });
