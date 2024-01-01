ebfc1ae808db9a464d2e245d6afdb3a0feacdfdf
0-BSCode <bryanipsanchez@gmail.com>
2024-01-01 17:32:38 +0800

    Add prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000; // Or get port from environment variable
 
-const test = 3;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi from Express + TypeScript!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi from Express + TypeScript!");
 });
 
 app.listen(port, () => {
